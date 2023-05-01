5e9a1ba973841c9df8af68e95f0e9138b572cae7
Oleksii Rozhko <rozhkoaleks@gmail.com>
2023-05-01 20:56:33 +0300

    lesson37 edit class to className

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,27 +3,27 @@ export const Experience = () => {
     <div className="experience">
       <h2>Work Experience</h2>
       <p>
-        <span class="bold">Name of employer:</span> Nuga Best Health Ltd.
+        <span className="bold">Name of employer:</span> Nuga Best Health Ltd.
         <br />
-        <span class="bold">Dates of employment:</span> 2018 - present
+        <span className="bold">Dates of employment:</span> 2018 - present
         <br />
-        <span class="bold">Job title: </span>CEO at Nuga Best Health Ltd., Co{' '}
+        <span className="bold">Job title: </span>CEO at Nuga Best Health Ltd., Co{' '}
         <a href="https://nugabestbg.com">https://nugabestbg.com</a>
         <br />
-        <span class="bold">Description:</span> Sales and personnel management up
+        <span className="bold">Description:</span> Sales and personnel management up
         to 5 people. Store financial management.
       </p>
 
-      <p class="experience-last">
-        <span class="bold">Name of employer:</span> Nuga Best Global Bulgaria
+      <p className="experience-last">
+        <span className="bold">Name of employer:</span> Nuga Best Global Bulgaria
         Ltd.
         <br />
-        <span class="bold">Dates of employment:</span> 2015 - 2018
+        <span className="bold">Dates of employment:</span> 2015 - 2018
         <br />
-        <span class="bold">Job title: </span>Sales consultant at{' '}
+        <span className="bold">Job title: </span>Sales consultant at{' '}
         <a href="https://nugabest.bg">Nuga Best Global Bulgaria Ltd.</a>
         <br />
-        <span class="bold">Description:</span>Consulting of South Korean massage
+        <span className="bold">Description:</span>Consulting of South Korean massage
         equipment. Sales of South Korean massage equipment.
       </p>
     </div>
